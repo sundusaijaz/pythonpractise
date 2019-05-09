fcf166736988e3aec97b1c1c736919e021e27ae8
sundusaijaz <sundusaijaz2011@gmail.com>
2019-05-09 04:33:20 -0700

    First commit

--- num_check.py
+++ num_check.py
@@ -3,3 +3,13 @@ if  num % 2 == 0:
     print("The given number is even")
 else:
     print("The given number is odd")
+
+
+
+
+
+
+
+
+
+
--- phopho.py
+++ phopho.py
@@ -1,6 +1,7 @@
 event_venue = input("where is event")
-if event_venue == 'phoopho Home':
+if event_venue == 'phoopho home':
     print("no thanks")
+    print("bye")
 else:
     print("Sure i will go")
 
